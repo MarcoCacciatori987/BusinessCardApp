c961caa69c86445989bf456c8f864ecae0460aff
Marco Cacciatori <marco.cacciatori@studenti.unimi.it>
2022-04-21 14:53:58 +0200

    agg

---
+++ Components/Metodi.jsx
@@ -0,0 +1,5 @@
+export default class Metodi {
+  static metodo1() {
+    return "ciao";
+  }
+}
--- Components/Prova.jsx
+++ Components/Prova.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 import { View, Text } from "react-native";
+import Metodi from "./Metodi";
 
 export default class Prova extends React.Component {
   state = {
-    id: "",
+    id: "tibe",
     num: [],
   };
+
+  constructor() {
+    super();
+    this.state.id = "ciambella";
+
+    this.setState(this.state);
+  }
 
   render() {
     return (
       <View>
-        <Text>ciao</Text>
+        <Text>ciao gay {Metodi.metodo1()}</Text>
       </View>
     );
   }
