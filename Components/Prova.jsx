88db12d01565b5c296d758a6e3b45d0f8a9fb6b4
Marco Cacciatori <marco.cacciatori@studenti.unimi.it>
2022-04-21 14:26:33 +0200

    ciao

---
+++ Components/Prova.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default class Prova extends React.Component {
+  state = {
+    id: "",
+    num: [],
+  };
+
+  render() {
+    return (
+      <View>
+        <Text>ciao</Text>
+      </View>
+    );
+  }
+}
